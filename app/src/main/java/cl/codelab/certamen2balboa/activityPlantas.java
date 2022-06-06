c72db74991a03c073f6b50ee52713f84411d8d1a
Patricio Balboa <69600943+patobalboa@users.noreply.github.com>
2022-06-06 03:37:01 -0400

    Otra vez 2323

---
+++ app/src/main/java/cl/codelab/certamen2balboa/activityPlantas.java
@@ -0,0 +1,14 @@
+package cl.codelab.certamen2balboa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activityPlantas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plantas);
+    }
+}
---
+++ app/src/main/java/cl/codelab/certamen2balboa/activityRecoleccion.java
@@ -0,0 +1,14 @@
+package cl.codelab.certamen2balboa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activityRecoleccion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recoleccion);
+    }
+}
