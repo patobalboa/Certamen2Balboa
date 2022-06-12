130e5320d569b4b2025b79fdfbd26ef86dfa9f01
Patricio Balboa <69600943+patobalboa@users.noreply.github.com>
2022-06-12 10:11:24 -0400

    Autor Item Menu

--- app/src/main/java/cl/codelab/certamen2balboa/Autor.java
+++ app/src/main/java/cl/codelab/certamen2balboa/Autor.java
@@ -1,8 +1,13 @@
 package cl.codelab.certamen2balboa;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class Autor extends AppCompatActivity {
 
@@ -10,5 +15,20 @@ public class Autor extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_autor);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater= getMenuInflater();
+        inflater.inflate(R.menu.menu_autor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()== R.id.itemPrincipal){
+            startActivity(new Intent(Autor.this, MainActivity.class));
+        }
+        return true;
     }
 }
